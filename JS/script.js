51caf9d167b305312268bf22e20cb0bace94f4c4
MatheRDias <matheusrdias@gmail.com>
2024-12-12 21:22:07 -0300

    Inplementação javascript

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+const login = document.getElementById('nome');
+const senha = document.getElementById('password');
+
+
+
+
+function validarUsuario()
+{
+    console.log(login.value + senha.value);
+    window.location.href = '../view/cadastro.html';
+  
+    
+
+}
